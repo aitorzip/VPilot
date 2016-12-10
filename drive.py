f606b59f3be090d530356fc76631618bb7f45bd5
Aitor Ruano <aitor.bak@gmail.com>
2016-12-10 17:49:18 +0100

    First silly working version

--- drive.py
+++ drive.py
@@ -1,70 +1,72 @@
 import argparse
-import base64
-
-import numpy as np
-import socketio
-import eventlet
-import eventlet.wsgi
 from PIL import Image
-from flask import Flask
-from io import BytesIO
+import socket, struct
+import numpy as np
+from array import array
+import model
 
-from keras.models import model_from_json
-from scipy.misc import imresize
+class Server:
+	def __init__(self, port=8000, image_size=(200,66)):
+		print('Started server')
+		self.image_size = image_size
+		self.buffer_size = image_size[0]*image_size[1]*3;
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.bind(('127.0.0.1', port))
+		self.s.listen(1)
 
+		self.conn, self.addr = self.s.accept()
+		print('GTAV connected')
 
-sio = socketio.Server()
-app = Flask(__name__)
-model = None
-prev_image_array = None
+	def recvImage(self):
+		data = b""
+		while len(data) < self.buffer_size:
+			packet = self.conn.recv(self.buffer_size - len(data))
+			if not packet: return None
+			data += packet
 
-@sio.on('telemetry')
-def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
-    # The current throttle of the car
-    throttle = data["throttle"]
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image = imresize(image,(200,66)).astype('float32')
-    image_array = np.asarray(image)
-    transformed_image_array = image_array[None, :, :, :]
-    # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2
-    print(steering_angle, throttle)
-    send_control(steering_angle, throttle)
+		print('Received image')
+		return ((np.array(Image.frombytes('RGB', (self.image_size[1], self.image_size[0]), data)).astype('float32'))[None,:,:,:])
 
+	def sendCommands(self,throttle, brake, steering):
+		data = array('f', [throttle, brake, steering])
+		self.conn.sendall(data.tobytes())
+		print('Sent commands')
 
-@sio.on('connect')
-def connect(sid, environ):
-    print("connect ", sid)
-    send_control(0, 0)
+	def recvReward(self):
+		data = b""
+		while len(data) < 4:
+			packet = self.conn.recv(self.buffer_size - len(data))
+			if not packet: return None
+			data += packet
 
+		print('Received reward')
+		return struct.unpack('f', data)[0]
 
-def send_control(steering_angle, throttle):
-    sio.emit("steer", data={
-    'steering_angle': steering_angle.__str__(),
-    'throttle': throttle.__str__()
-    }, skip_sid=True)
-
+	def close(self):
+		self.s.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
-    args = parser.parse_args()
-    with open(args.model, 'r') as jfile:
-        model = model_from_json(jfile.read())
+	parser = argparse.ArgumentParser(description='Remote Driving')
+	parser.add_argument('model', type=str, help='Path to model definition json. Model weights should be on the same path.')
+	parser.add_argument('port', type=int, help='Port to listen to')
+	parser.add_argument('width', type=int, help='Width of the image to receive')
+	parser.add_argument('height', type=int, help='Height of the image to receive')
+	args = parser.parse_args()
+	model = model.getModel(model_path=args.model)
 
-    model.compile("adam", "mse")
-    weights_file = args.model.replace('json', 'h5')
-    model.load_weights(weights_file)
+	server = Server(port=args.port, image_size=(args.width, args.height))
+	while 1:
+		image = server.recvImage()
+		if (image == None): break
+		throttle = 1.0
+		brake = 0.0
+		steering = float(model.predict(image, batch_size=1))
+		server.sendCommands(throttle, brake, steering)
+		reward = server.recvReward()
+		if (reward == None): break
+		print(reward)
 
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
+	server.close()
 
-    # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+
+   
--- model.py
+++ model.py
@@ -1,78 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Dec  6 18:22:21 2016
 
-@author: aitor.
-"""
-from keras.models import Sequential
+from keras.models import Sequential, model_from_json
 from keras.layers import Flatten, Dense, Dropout, ELU
 from keras.layers.convolutional import Convolution2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.core import  Lambda
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
-from keras.models import model_from_json
-
-import numpy as np
-from scipy.misc import imread, imresize
-import csv
-import cv2
-import os
-
-#Data generator to save memory resources
-def data_generator(image_paths, steering_angles, batch_size, img_size=None):  
-    while 1:
-        img_list = []
-        steering_angles_list = []
-        i = 0
-        for im_path in image_paths:
-            img = imread(im_path)
-            if img_size:
-                img = imresize(img,img_size)
-            img = img.astype('float32')
-    
-            img_list.append(img)
-            steering_angles_list.append(steering_angles[i])
-            
-            #extra images (double), flip horizontally
-            img_list.append(cv2.flip(img,1))
-            steering_angles_list.append(-steering_angles[i])
-            
-            i = i+1
-            
-            if((i % batch_size) == 0):
-                #shuffle the batch
-                rng_state = np.random.get_state()
-                np.random.shuffle(img_list)
-                np.random.set_state(rng_state)
-                np.random.shuffle(steering_angles_list)
-                
-                img_batch = np.stack(img_list, axis=0)
-                steering_angles_batch = np.array(steering_angles_list)
-                yield(img_batch, steering_angles_batch)
-                img_list.clear()
-                steering_angles_list.clear()  
-                
-                
-            
-    
-
 
 #Based on:
 #   75% NVIDIA's end-to-end paper: https://arxiv.org/pdf/1604.07316v1.pdf
 #   25% Comma.ai research: https://github.com/commaai/research/blob/master/SelfSteering.md
-def getNNModel(model_path=None):
+def getModel(model_path=None):
       
     if model_path:
         with open(model_path, 'r') as jfile:
             model = model_from_json(jfile.read())
+            weights_path = model_path.replace('json', 'h5')
+            model.load_weights(weights_path)
     else:
-
-        ch, width, height = 3, 66, 200
+        width, height, ch = 200, 66, 3
         
         model = Sequential()
-        model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(height, width, ch), output_shape=(height, width, ch)))
+        model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(width, height, ch), output_shape=(width, height, ch)))
         
         model.add(Convolution2D(24, 5, 5, subsample=(2,2), border_mode='same', init='he_normal', name='conv1'))
         model.add(ELU())
@@ -108,73 +57,3 @@ def getNNModel(model_path=None):
         model.add(Dense(1, init='he_normal', name='output'))
 
     return model
-    
-
-#Load dataset
-image_paths = []
-steering_angles = []
-with open('Center/driving_log.csv', 'r') as csvfile:
-    dataset = csv.reader(csvfile)
-    for row in dataset:
-        image_paths.append(row[0].strip())
-        image_paths.append(row[1].strip())
-        image_paths.append(row[2].strip())
-        steering_angles.append(float(row[3]))
-        steering_angles.append(float(row[3]) + 0.2)
-        steering_angles.append(float(row[3]) - 0.2)
-        
-with open('Right offset/driving_log.csv', 'r') as csvfile:
-    dataset = csv.reader(csvfile)
-    for row in dataset:
-        if (float(row[3]) < 0.0): #only recoveries!
-            image_paths.append(row[0].strip())
-            image_paths.append(row[1].strip())
-            image_paths.append(row[2].strip())
-            steering_angles.append(float(row[3]))
-            steering_angles.append(float(row[3]) + 0.2)
-            steering_angles.append(float(row[3]) - 0.2)
-            
-with open('Left Offset/driving_log.csv', 'r') as csvfile:
-    dataset = csv.reader(csvfile)
-    for row in dataset:
-        if (float(row[3]) > 0.0): #only recoveries!
-            image_paths.append(row[0].strip())
-            image_paths.append(row[1].strip())
-            image_paths.append(row[2].strip())
-            steering_angles.append(float(row[3]))
-            steering_angles.append(float(row[3]) + 0.2)
-            steering_angles.append(float(row[3]) - 0.2)
-
-
-#shuffle the dataset
-rng_state = np.random.get_state()
-np.random.shuffle(image_paths)
-np.random.set_state(rng_state)
-np.random.shuffle(steering_angles)
-
-#Split into training and validation, 80/20%
-dataset_length = len(steering_angles)
-train_image_paths = image_paths[0:int(dataset_length*0.8)]
-train_steering_angles = steering_angles[0:int(dataset_length*0.8)]
-val_image_paths = image_paths[int(dataset_length*0.8):dataset_length]
-val_steering_angles = steering_angles[int(dataset_length*0.8):dataset_length]
-    
-#Training parameters                                    
-adam = Adam(lr=0.001)
-model = getNNModel(model_path="model.json")
-if os.path.exists("model.h5"):
-    model.load_weights("model.h5")
-model.compile(optimizer=adam, loss='mse')
-
-#Trains indefinetely and saves best model (lower validation loss)
-checkpoint_callback = ModelCheckpoint("model.h5", monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min')
-train_generator = data_generator(train_image_paths, train_steering_angles, 42, img_size=(200,66))
-val_generator = data_generator(val_image_paths, val_steering_angles, 162, img_size=(200,66))
-model.fit_generator(
-                    train_generator, 
-                    samples_per_epoch=2*len(train_steering_angles), #double, because we flip images horizontally in the generator
-                    nb_epoch=1000,
-                    validation_data=val_generator,
-                    nb_val_samples=2*len(val_steering_angles), 
-                    callbacks=[checkpoint_callback]
-)
