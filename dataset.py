47ca40a7b088c2507000f7e217c514285f90bc4a
Christopher Geier <cpg3rb@virginia.edu>
2017-06-02 17:38:53 -0400

    Cleaned up comments and added normal driving style
    Don't really see what the point of having a random driving style is. Took me forever to figure out why my car kept going backwards. Other driving styles can be found at http://gtaforums.com/topic/822314-guide-driving-styles/

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ import argparse
 import time
 import cv2
 
-#Stores a dataset file with data coming from DeepGTAV
+# Stores a dataset file with data coming from DeepGTAV
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description=None)
 	parser.add_argument('-l', '--host', default='localhost', help='The IP where DeepGTAV is running')
@@ -16,30 +16,31 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--dataset_path', default='dataset.pz', help='Place to store the dataset')
 	args = parser.parse_args()
 
-	#Creates a new connection to DeepGTAV using the specified ip and port. 
-	#If desired, a dataset path and compression level can be set to store in memory all the data received in a gziped pickle file.
+	# Creates a new connection to DeepGTAV using the specified ip and port. 
+	# If desired, a dataset path and compression level can be set to store in memory all the data received in a gziped pickle file.
 	client = Client(ip=args.host, port=args.port, datasetPath=args.dataset_path, compressionLevel=9)
 	
-	#Configures the information that we want DeepGTAV to generate and send to us. 
-	#See deepgtav/messages.py to see what options are supported
+	# Configures the information that we want DeepGTAV to generate and send to us. 
+	# See deepgtav/messages.py to see what options are supported
 	dataset = Dataset(rate=30, frame=[320,160], throttle=True, brake=True, steering=True, vehicles=True, peds=True, reward=[15.0, 0.0], direction=None, speed=True, yawRate=True, location=True, time=True)
-	#Send the Start request to DeepGTAV. Scenario is set as default (random weather, position, time and driving style)
-	client.sendMessage(Start(dataset=dataset))
+	# Send the Start request to DeepGTAV.
+	scenario = Scenario(drivingMode=[786603,15.0]) # Driving style is set to normal, with a speed of 15.0 mph. All other scenario options are random.
+	client.sendMessage(Start(dataset=dataset,scenario=scenario))
 
-	#Start listening for messages coming from DeepGTAV. We do it for 80 hours
+	# Start listening for messages coming from DeepGTAV. We do it for 80 hours
 	stoptime = time.time() + 80*3600
 	while time.time() < stoptime:
 		try:
-			#We receive a message as a Python dictionary
+			# We receive a message as a Python dictionary
 			message = client.recvMessage()	
 			
-			#The frame is a numpy array and can be displayed using OpenCV or similar		
-			#image = frame2numpy(message['frame'], (320,160))
-			#cv2.imshow('img',image)
-			#cv2.waitKey(-1)
+			# The frame is a numpy array and can be displayed using OpenCV or similar		
+			# image = frame2numpy(message['frame'], (320,160))
+			# cv2.imshow('img',image)
+			# cv2.waitKey(-1)
 		except KeyboardInterrupt:
 			break
 			
-	#We tell DeepGTAV to stop
+	# We tell DeepGTAV to stop
 	client.sendMessage(Stop())
 	client.close()
